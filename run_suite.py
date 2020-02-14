4bc3b0e04e925dcd93ef655d100cf1a8b0c1508d
lixiaoxuan <927332786@qq.com>
2020-02-14 19:33:16 +0800

    zzz

--- run_suite.py
+++ run_suite.py
@@ -4,7 +4,6 @@ from tools.HTMLTestRunner import HTMLTestRunner
 # 组装测试套件
 suite= unittest.defaultTestLoader.discover("./scripts",pattern="test*.py")
 
-# 获取报告存储文件夹 实例化HTMLTestRunner 调用run执
-# 行测试套  件
-with open("./report/ihrm_report.html","wb") as f:
+# 获取报告存储文件夹 实例化HTMLTestRunner 调用run执 行测试套  件
+with open("./report/report.html","wb") as f:
     HTMLTestRunner(stream=f).run(suite)
